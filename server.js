b1dced756878abc939fdbe5f8af5c9503197a059
aagneye-syam <saagneye2003@gmail.com>
2024-01-17 17:46:42 +0530

    dotenv setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = 5000;
+
+app.listen(port, ()=>{
+        console.log('Server running on the port 5000');
+})
