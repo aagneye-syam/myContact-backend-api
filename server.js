8e3a80cf97029c387d66cd09270c7475a46397c8
aagneye-syam <saagneye2003@gmail.com>
2024-01-19 21:22:17 +0530

    config thunder client

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get('/api/contact',(req,res)=>{
+        res.send('Get all contacts');
+});
+
 app.listen(port, ()=>{
         console.log('Server running on the port '+port);
 })
